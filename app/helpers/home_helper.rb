f9f829412c2cab9ab60aaff3d78ec21108d87ef1
Hiro Inagaki <hironori.ingk@gmail.com>
2019-06-07 01:02:24 +0900

    Add logout feature

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -10,4 +10,12 @@ module HomeHelper
       redirect_to root_path
     end
   end
+
+  def current_user
+    @current_user
+  end
+
+  def reset_session
+    session['userinfo'] = nil if session['userinfo'].present?
+  end
 end
