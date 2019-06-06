646b824468ba6b4cd2186ef789752e0924f27081
Hiro Inagaki <hironori.ingk@gmail.com>
2019-06-07 00:12:20 +0900

    Add bootstrap and auth0

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get 'dashboard' => 'home#dashboard'
+
+  get 'auth/auth0', as: 'authentication'        # Triggers authentication process
+  get 'auth/auth0/callback' => 'auth0#callback' # Authentication successful
+  get 'auth/failure' => 'auth0#failure'         # Authentication fail
 end
